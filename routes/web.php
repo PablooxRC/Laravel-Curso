4e4d376fc8d5ffde6d9888ac3f5dd3807f6b4222
Pablo Rodriguez <pabloox73@gmail.com>
2026-03-10 18:42:17 -0400

    rutas

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//Acceso a las vistas sin necesidad de crear un controlador, solo con la ruta y la vista
+//URL, vista, nombre de la ruta
+Route::view('/', 'welcome')->name('home');;
+Route::view('contacto', 'contact')->name('contact');
+Route::view('blog', 'blog')->name('blog');
+Route::view('nosotros', 'about')->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
